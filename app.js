f92f5f1ee62b533d5e283fb5988caac54520bf4e
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-09 12:53:21 +0500

    Selet all UI elements

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// SELECT UI ELEMENTS
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const dotsEl = document.querySelector(".dots");
+
+const dayEl = document.querySelector(".day");
+const dateEl = document.querySelector(".date");
+const yearEl = document.querySelector(".year");
